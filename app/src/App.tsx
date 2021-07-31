b32afb77fed38df02809be9525b8bb73f4cc0e76
Andreas Petersen (Guldberg) <andreasgdp@gmail.com>
2021-07-31 14:00:52 +0200

    Working login

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -10,6 +10,12 @@ import Settings from './components/settings/settings';
 import Popup from './components/popup/Popup';
 import './App.scss';
 
+const user = {
+	id: 9272,
+	color: 30,
+	email: "test@test.test",
+	pass: "123456",
+};
 
 function App() {
 	const navIndex = useSelector((state: RootState) => state.navState.index);
@@ -17,24 +23,17 @@ function App() {
 	const isLoggedIn = useSelector((state: RootState) => state.session.isLoggedIn);
 	const dispatch = useDispatch();
 
-	const user = {
-		id: 9272,
-		color: 30,
-		email: "test@test.test",
-		pass: "123456",
-	};
-
 	((hue: number) =>
 		document.documentElement.style.setProperty('--c', String(hue)))(
 		user.color
 	);
 
 	const updateDataEmail = (e: any) => {
-		user.email = ({value: "", ...e.target}).value
+		user.email = e.target.value
 	};
 
 	const updateDataPass = (e: any) => {
-		user.pass = ({value: "", ...e.target}).value
+		user.pass = e.target.value
 	};
 
 	return (
@@ -44,18 +43,18 @@ function App() {
 					{ userPopup === 1 ? <>
 						<h1>Login/Signup</h1>
 						<p>Enter email, if its is not in our system, you will be redirected to signup.</p>
-						<input type={"email"} placeholder={"test@test.test"} id={"userCheckEmail"} onInput={updateDataEmail} />
+						<input type={"email"} placeholder={"test@test.test"} id={"userCheckEmail"} onChange={updateDataEmail} />
 						<div onClick={() => dispatch(checkForUser(user.email))}>Check email</div>
 					</> : userPopup === 2 ? <>
 						<h1>Login</h1>
 						<p>Mail known, enter password</p>
-						<input type={"password"} placeholder={"123456"} onInput={updateDataPass}/>
+						<input type={"password"} placeholder={"123456"} onChange={updateDataPass}/>
 						<div onClick={() => dispatch(userLogin(user.email, user.pass))}>Login</div>
 					</> : userPopup === 3 ? <>
 						<h1>Signup</h1>
 						<p>Email not known, enter verification code from mail</p>
 						<input type={"text"} placeholder="222444"/>
-						<div onClick={() => dispatch(createUser(user.email, user.pass, user.pass))}>Login</div>
+						<div onClick={() => {dispatch(createUser(user.email, user.pass, user.pass))}}>Login</div>
 					</> : <></> }
 				</Popup>
 			: <></> }
