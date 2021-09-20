81e2fe4b0673ffa0b7b86c1261ebbf508c593a85
Victor Vejlgaard Schelde Jacobsen <victor.jacobsen@outlook.dk>
2021-09-20 21:29:58 +0200

    codefactor fixes

--- app/src/components/settings/DesignPreview.tsx
+++ app/src/components/settings/DesignPreview.tsx
@@ -29,9 +29,9 @@ const setcolor = (base: number, shade: number) => {
   document.documentElement.style.setProperty('--e', shade+"%");
 };
 
-const iterateHTMLCollection = (a: HTMLCollection, fnc: any) => {
+/* const iterateHTMLCollection = (a: HTMLCollection, fnc: any) => {
   for (let i = 0; i < a.length; i++) fnc(a.item(i));
-}
+} */
 
 const DOMStringMapToObj = (dsm: DOMStringMap) => {
   const obj: any = {};
