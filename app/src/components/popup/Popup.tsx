af6db07dfbae43c907dee84488fc639050a2f1a8
Victor Vejlgaard Schelde Jacobsen <victor.jacobsen@outlook.dk>
2021-10-24 15:07:03 +0200

    Login done (#145)
    * Login done
    
    * sonarcloud fix

--- app/src/components/popup/Popup.tsx
+++ app/src/components/popup/Popup.tsx
@@ -1,17 +1,22 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { setUserPopup } from '../../reducers/navState';
 import './Popup.scss';
 
 interface PopupProps {
   type: string,
+  dispatch: Function,
+  navCollapsed: boolean,
 }
 
 //Creates a popup container
  class Popup extends Component<PopupProps> {
   render() {
-    const {type, children} = this.props;
+    const {type, children, dispatch, navCollapsed} = this.props;
     return (
-      <div id={type} className={"popup"}>
-        {children}
+      <div className={"popup"} onClick={() => dispatch(setUserPopup(0))}>
+        <div id={type} onClick={(evt)=>evt.stopPropagation()} className={navCollapsed?"close":"far"}>
+          {children}
+        </div>
       </div>
     );
   }
